e181a28e78f275c3484a070fbedf9dd30345d621
HoangLinh2k6 <do16092006@gmail.com>
2025-10-29 19:33:34 +0700

    Sửa một số thứ

--- Project/src/main/java/arkanoid/engine/Define.java
+++ Project/src/main/java/arkanoid/engine/Define.java
@@ -1,7 +1,7 @@
 package main.java.arkanoid.engine;
 //lưu các địa chỉ file hoặc các hằng số ở đây để tiện sử dụng
 public class Define {
-    public static final String MULTI_BALL_IMAGE_PATH        = "/com/Arkanoid/img/stone.png";
+    public static final String MULTI_BALL_IMAGE_PATH        = "/com/Arkanoid/img/MultiBall.png";
     public static final String PADDLES_AND_BALLS_IMAGE_PATH = "/com/Arkanoid/img/paddles_and_balls.png";
     public static final String BRICKS_IMAGE_PATH            = "/com/Arkanoid/img/bricks.png";
     public static final String END_SCREEN_PATH              = "/com/Arkanoid/EndScreen.fxml";
